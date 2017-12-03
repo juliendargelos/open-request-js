cc8bda8abcc0db9475c1ac6a901f91426183d831
Julien Dargelos <julien.dargelos@me.com>
2017-12-03 13:04:25 +0100

    Improvements

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
+if('require' in this) {
+  var Parameters = require('parameters-js');
+  var Url = require('open-url-js');
+}
+
 class Request {
   constructor() {
-    try {
-      if(typeof require !== 'function') require = function() {};
-    }
-    catch(e) {
-      var require = function() {};
-    }
-    if(!Parameters) Parameters = require('parameters-js');
-    if(!Url) Url = require('open-url-js');
+
   }
 }
 
-if(typeof module !== 'object' || module === null) module = {};
-module.exports = Request;
+if('module' in this) module.exports = Request;
